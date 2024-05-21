20f5161591271f48bc859a014268b83a77643190
Anupama Suresh <anupamasuresh812@gmail.com>
2024-05-21 13:10:08 +0530

    palindrome - checks whether the input string is a palindrome or not

---
+++ palindrome.py
@@ -0,0 +1,10 @@
+def palindrome(s):
+    count=len(s)
+    p_str=""
+    for i in range(count-1,-1,-1):
+        p_str+=s[i]
+    print("palindrome" if(s==p_str) else "not palindrome")
+
+if __name__ =='__main__':
+    s=input("Enter a string\n")
+    palindrome(s)
